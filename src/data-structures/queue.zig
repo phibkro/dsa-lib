94578c62e10db095dc62e704844878311fc0a20a
phibkro <philib.krogh@gmail.com>
2024-06-17 07:19:23 +0200

    feat: :sparkles: implement Queue
    Implement a Queue following official Zig code example: https://ziglang.org/learn/samples/

---
+++ src/data-structures/queue.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+const testing = std.testing;
+
+pub fn Queue(comptime T: type) type {
+    return struct {
+        const This = @This();
+        const Node = struct {
+            value: T,
+            next: ?*Node,
+        };
+        allocator: std.mem.Allocator,
+        start: ?*Node,
+        end: ?*Node,
+
+        pub fn init(allocator: std.mem.Allocator) This {
+            return This{
+                .allocator = allocator,
+                .start = null,
+                .end = null,
+            };
+        }
+        pub fn enqueue(this: *This, value: T) !void {
+            const node = try this.allocator.create(Node);
+            node.* = .{ .value = value, .next = null };
+            if (this.end) |end| {
+                end.next = node;
+            } else {
+                this.start = node;
+            }
+            this.end = node;
+        }
+        pub fn dequeue(this: *This) ?T {
+            const start = this.start orelse return null;
+            defer this.allocator.destroy(start);
+            if (start.next) |next|
+                this.start = next
+            else {
+                this.start = null;
+                this.end = null;
+            }
+            return start.value;
+        }
+    };
+}
+
+test "queue" {
+    var int_queue = Queue(i32).init(testing.allocator);
+
+    try int_queue.enqueue(25);
+    try int_queue.enqueue(50);
+    try int_queue.enqueue(75);
+    try int_queue.enqueue(100);
+
+    try testing.expectEqual(int_queue.dequeue(), 25);
+    try testing.expectEqual(int_queue.dequeue(), 50);
+    try testing.expectEqual(int_queue.dequeue(), 75);
+    try testing.expectEqual(int_queue.dequeue(), 100);
+    try testing.expectEqual(int_queue.dequeue(), null);
+
+    try int_queue.enqueue(5);
+    try testing.expectEqual(int_queue.dequeue(), 5);
+    try testing.expectEqual(int_queue.dequeue(), null);
+}
